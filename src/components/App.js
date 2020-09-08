95a670af4b3fc7de2033470c8ec70d5eea99507c
Allison Sadin <aesadin@gmail.com>
2020-09-08 13:19:52 -0700

    create file tree
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>
    Co-authored-by: James Henager <jameschenager@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
