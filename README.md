d4a186753b429455be41e7d61e31033e7c9c8748
Allison Sadin <aesadin@gmail.com>
2020-09-09 14:33:27 -0700

    add picture to readme
    Co-authored-by: Tyson Lackey <lackeyt90@gmail.com>
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+![ticketDiagram](./public/ticketdiagram.png)
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -2,11 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Ticket(props){ // for each ticket its passing in individual properties for a single ticket creation
+  const myTicketStyle = {
+    backgroundColor: 'blue',
+    fontFamily: 'sans-serif',
+    textAlign: 'center'
+  }
+
+
   return (
     <React.Fragment> 
-      <h3>{props.location} - {props.names}</h3>                        
-      <p><em>{props.issue}</em></p>
-      <hr/>
+      <div style = {myTicketStyle}>
+        <h3>{props.location} - {props.names}</h3>                        
+        <p><em>{props.issue}</em></p>
+        <hr/>
+      </div>
     </React.Fragment>
   ); //this react fragment tells you how it looks on the page
 }
