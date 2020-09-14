58e4b6393e76ede72ca44f9bd0cb067514bb7331
Allison Sadin <aesadin@gmail.com>
2020-09-14 10:13:46 -0700

    pass second test
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -2,7 +2,7 @@ import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
 
-//   let action;
+  let action;
 
 //   const currentState = {
 //     1: {names: 'Ryan & Aimen',
@@ -15,35 +15,36 @@ describe('ticketListReducer', () => {
 //     id: 2 }
 //   }
 
-//   const ticketData = {
-//     names: 'Ryan & Aimen',
-//     location: '4b',
-//     issue: 'Redux action is not working correctly.',
-//     id: 1
-//   };
+  const ticketData = {
+    names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1
+  };
 
   test('Should return default state if no action type is recognized', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
-//   test('Should successfully add new ticket data to masterTicketList', () => {
-//     const { names, location, issue, id } = ticketData;
-//     action = {
-//       type: 'ADD_TICKET',
-//       names: names,
-//       location: location,
-//       issue: issue,
-//       id: id
-//     };
-//     expect(ticketListReducer({}, action)).toEqual({
-//       [id] : {
-//         names: names,
-//         location: location,
-//         issue: issue,
-//         id: id
-//       }
-//     });
-//   });
+  test('Should successfully add new ticket data to masterTicketList', () => {
+    const { names, location, issue, id } = ticketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+    
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+  });
 
 //   test('Should successfully delete a ticket', () => {
 //     action = {
--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,24 +1,21 @@
-export default (state = {}, action) => {
-  return state;
-}
 
-// export default (state = {}, action) => {
-//   const { names, location, issue, id } = action;
-//   switch (action.type) {
-//   case 'ADD_TICKET':
-//     return Object.assign({}, state, {
-//       [id]: {
-//         names: names,
-//         location: location,
-//         issue: issue,
-//         id: id
-//       }
-//     });
-//   case 'DELETE_TICKET':
+export default (state = {}, action) => {
+  const { names, location, issue, id } = action;
+  switch (action.type) { // kind of like an if/else statement
+  case 'ADD_TICKET':     // if the case is add ticket then do the following...
+    return Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+//   case 'DELETE_TICKET': // else if the case is delete ticket then do this...
 //     const newState = { ...state };
 //     delete newState[id];
 //     return newState;
-//   default:
-//     return state;
-//   }
-// };
+  default:
+    return state;
+  }
+};
