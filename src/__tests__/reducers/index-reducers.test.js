a1e752dca6d7eaa42199bfa712e97de4a501d737
Kevin Davis <thekidnamedkd@gmail.com>
2020-09-15 11:03:29 -0700

    pass initial index reducer test

--- src/__tests__/reducers/index-reducers.test.js
+++ src/__tests__/reducers/index-reducers.test.js
@@ -1,10 +1,13 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
   test('Should return default state if no action is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
-      masterticketList: {},
+      masterTicketList: {},
       formVisibleOnPage: false
     });
   });
